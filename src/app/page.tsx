adf4de9f76243392973b5fca271a140b4799779d
Chibuzor-Egbo <chibuzoregbo04@gmail.com>
2024-09-11 15:21:06 +0100

    first stages

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Link } from "nextra-theme-docs";
+
+function page() {
+  return (
+    <>
+      <Link href="/getting-started">see docs</Link>
+    </>
+  );
+}
+
+export default page;
---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+export default {
+  logo: <span>Tranzakt</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+    </>
+  ),
+  // ... other theme options
+};
